74dd9886f471ea6914b92353cfcdaded38bbf67b
esdmr <esdmr0@gmail.com>
2024-11-09 11:19:47 +0330

    fix(time): use inspect for LogMessage

--- notepadd-timekeeper/src/output.ts
+++ notepadd-timekeeper/src/output.ts
@@ -1,10 +1,18 @@
 import process from 'node:process';
+import {inspect} from 'node:util';
 import {TimekeeperMessage} from './messages/index.ts';
 import {LogMessage, type LogLevel} from './messages/log.ts';
 
 function createLogger(level: LogLevel) {
 	return (...items: unknown[]) => {
-		process.send!(new TimekeeperMessage(new LogMessage(level, items)));
+		process.send!(
+			new TimekeeperMessage(
+				new LogMessage(
+					level,
+					items.map((i) => (typeof i === 'string' ? i : inspect(i))),
+				),
+			),
+		);
 	};
 }
 
