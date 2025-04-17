00e183c59843e667125db40bc3e380179864cc30
esdmr <esdmr0@gmail.com>
2025-04-17 19:50:57 +0330

    fix(vsc/v/past-alarms): show prev instant instead

--- vscode-notepadd/src/view/past-alarms.ts
+++ vscode-notepadd/src/view/past-alarms.ts
@@ -21,6 +21,19 @@ export class BridgeInstance extends BridgeDirective {
 		super(data);
 		this.id = `${this.id}@${String(data.instance.previous)}`;
 	}
+
+	override setState(state: DirectiveState): void {
+		super.setState(state);
+
+		// TODO: Make locale configurable. Also make the display
+		// timezone/calendar configurable.
+		//
+		// FIXME: This duplicates the calculation done in the overriden
+		// function.
+		this.description = state.instance.previous?.toLocaleString('en-GB', {
+			calendar: state.instance.previous.calendarId,
+		});
+	}
 }
 
 type PastAlarmsTreeItem = BridgeInstance;
